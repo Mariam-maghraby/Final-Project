bb9cdfaa63e4a267f604f3c3245317dcfbf82db5
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2020-05-22 05:22:23 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package com.Mcoding;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }mmm
-	    scanner
+    public static books() {
+	
+    }
+	    
 }
