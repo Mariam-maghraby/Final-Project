c0a115f66873ac95fb0108d5ec1fe0e141dc2f91
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2020-05-22 05:58:01 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ package com.Mcoding;
 public class Main {
 private String title, author, dateofpublish; 
 	
-    public void Book (String title, String author, String dateofpublish) {
+    public void Book (Scanner entering, String title, String author, String dateofpublish) {
+	    System.out.println("Enter any data about the book");
+	    Scanner entering = new Scanner(System.in);
 	    
 	    
-	// write your code here
+	
     }
 }
