20badc2563e1297ef8c5fd785b0b234d7154b110
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2020-05-22 09:46:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,43 @@
-package com.Mcoding;
+package CMPproject;
 
-public class book {
-private String title, author, dateofpublish;
-	Scanner entering = new Scanner(System.in);
-	
-    public Book () {
-	    System.out.println("Enter any data about the book");
-	    Scanner scan = new Scanner(System.in);
-	    String bookName = scan.next();
-	    
-	    
-	
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the no. of books needed to be entered:");
+        int arrayCols = scan.nextInt();
+        String array[][] = new String[3][arrayCols];
+        enterBooksData(scan, array, arrayCols);
+        printBookData(array, arrayCols);
+    }
+
+    public static void enterBooksData(Scanner scan1, String[][] matrix,  int matrixColumn) {
+        System.out.println("Enter the book data");
+        System.out.println("Enter the book name");
+        String bookName = scan1.next();
+        System.out.println("Enter the author name");
+        String authorName = scan1.next();
+        System.out.println("Enter the date of publishing");
+        String dateOfPublish = scan1.next();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < matrixColumn-1; j++) {
+                matrix[3][matrixColumn] = String.valueOf(scan1.nextInt());
+            }
+        }
+        System.out.println(matrix);
+    }
+
+    public static void printBookData(String[][] matrix, int matrixCols) {
+        System.out.println("The Data of the book: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < matrixCols; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+
+        }
     }
 }
+
